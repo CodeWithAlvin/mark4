01f24dab3d066ca755baddba03b0bde000bcf11d
Alvin saini <codewithalvin@gmail.com>
2020-10-18 15:08:35 +0530

    Add files via upload

---
+++ utills/data.py
@@ -0,0 +1,23 @@
+class query:
+		hey=["hey jarvis","hii jarvis","hey mark4","hii mark4","hey"]
+		wayd=["what are you doing","what's happening","what's happening"]
+		wcyd=["what can you do","what's your functionality"]
+		intro=["who are you","introduce yourself"]
+		tyme=["what's the time","tell me time","time please","what is the time","current time"]
+		date=["what's the date","tell me today's date","date please","today's date","today date","what is the date"]
+		hay=["how are you"]
+		video=["start video recording","record video"]
+		audio=["start audio recording","record audio"]
+		scan=["scan barcode","scan code"]
+		location=["where am i","my location","current location"]
+class answer:				
+		hey=["at your service sir ! ","I am here","Sir !"]
+		wayd=["waiting for your command , sir","i am Learning some new skills"," I am Exploring virtual world"]
+		wcyd=["it's a long list , let me start , I can record audio ,video and capture photo for you , I can send mails, I can make a call and message to your contacts, open apps search webs , I can tell uptime of system ,i can set alarms , timers , search Wikipedia , answer your basic queries ,tell you about system stats like battery ,tell your current coordinates ,scan barcodes , change settings like airplane mode silent mode and change volume"]
+		intro=["I am mark 4 , fourth version of project jarvis i am created by Alvin saini ,I am written in python and continuously improving with time"]
+		hay=["i am always fine","fine , hope you are good"]
+
+
+
+
+
